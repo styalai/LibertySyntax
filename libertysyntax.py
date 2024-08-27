67917fdfe0cc8a30469c2da6d22d8316a1918201
styalai <arthur.lagacherie@gmail.com>
2024-08-27 22:53:22 +0200

    commit

--- libertysyntax.py
+++ libertysyntax.py
@@ -2,6 +2,7 @@ from tqdm import tqdm
 import argparse
 import os
 from transformers import pipeline, AutoTokenizer, AutoModelForCausalLM, TextStreamer
+import torch
 
 def download_model(model_name="Arthur-LAGACHERIE/gemma-2-9b-it-4bits"):
     model = pipeline('text-generation', model=model_name)
@@ -56,22 +57,27 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("-file_name", help="the file to traduce", default=os.getcwd())
     parser.add_argument("-model_name", default="Arthur-LAGACHERIE/gemma-2-9b-it-4bits")
-    parser.add_argument("-device", default="cuda")
+    parser.add_argument("-device", default="cpu")
     parser.add_argument("-stream", default="False")
+    parser.add_argument("-token")
     args = parser.parse_args()
 
+    if args.device != "cuda":
+        args.model_name = "google/gemma-2-2b-it"
+    
     if args.stream == "True":
-        tokenizer = AutoTokenizer.from_pretrained(args.model_name, token="")
+        tokenizer = AutoTokenizer.from_pretrained(args.model_name, token=args.token)
         streamer = TextStreamer(tokenizer, skip_prompt=True)
 
         model = pipeline('text-generation', 
                         model=args.model_name,
                         tokenizer=tokenizer,
                         streamer=streamer,
-                        token="")
+                        token=args.token,
+                        device=args.device)
 
     else:
-        model = pipeline('text-generation', model=args.model_name, token="")
+        model = pipeline('text-generation', model=args.model_name, token=args.token, device=args.device)
     
 
     code_to_traduce = open(args.file_name, "r").read()
@@ -79,7 +85,7 @@ if __name__ == '__main__':
     code_traduced = traduce(model, code_to_traduce, device=args.device)
 
     write = open(args.file_name+".py", "a").write(code_traduced)
-    write.close()
+
 
 
 
